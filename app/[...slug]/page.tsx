25b95e01ccafe7e1a09074869506050f3b6a5cfa
David San Filippo <David.Filippo@edynamic.net>
2025-08-09 19:37:49 -0400

    Removed unused imports

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,18 +1,12 @@
-import RenderComponents from "@/components/render-components";
 import { Page } from "@/core/types/Page";
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
-import { getPage, initLivePreview } from "@/lib/contentstack"; // Importing functions to get page data and initialize live preview from a local library
-import ContentstackLivePreview, {
-  VB_EmptyBlockParentClass,
-} from "@contentstack/live-preview-utils"; // Importing live preview utilities from Contentstack
 import { GetPage } from "@/core/ContentQueries/GetPage"
 import { GetAllPages } from "@/core/ContentQueries/GetAllPages";
 import { notFound } from 'next/navigation';
 import FlexGrid from "@/components/flex-grid";
 import LivePreview from "@/components/LivePreview";
 
-export const revalidate = 0;
+export const timeout = parseInt(process.env.REVALIDATE_TIME_OUT || '0');
+export const revalidate = Number.isInteger(timeout) ? timeout : 0;
 
 export async function generateStaticParams() {
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
-import RenderComponents from "@/components/render-components";
-import { onEntryChange } from "@/contentstack-sdk";
-import { metaData } from "@/helper";
 import { Page } from "@/core/types/Page";
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
 import { notFound } from 'next/navigation';
-import { getPage, initLivePreview } from "@/lib/contentstack"; // Importing functions to get page data and initialize live preview from a local library
-import ContentstackLivePreview, {
-  VB_EmptyBlockParentClass,
-} from "@contentstack/live-preview-utils"; // Importing live preview utilities from Contentstack
-import Skeleton from "react-loading-skeleton";
 import { GetPage } from "@/core/ContentQueries/GetPage"
 import FlexGrid from "@/components/flex-grid";
 import LivePreview from "@/components/LivePreview";
