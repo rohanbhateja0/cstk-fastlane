30e69ac3bcf4ff0d75a7dc5c3708d8eb2714f1ef
David San Filippo <David.Filippo@edynamic.net>
2025-08-09 19:37:00 -0400

    Moved Revalidate to Env Variable

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,8 @@ import { GetPage } from "@/core/ContentQueries/GetPage"
 import FlexGrid from "@/components/flex-grid";
 import LivePreview from "@/components/LivePreview";
 
-export const revalidate = 0;
+export const timeout = parseInt(process.env.REVALIDATE_TIME_OUT || '0');
+export const revalidate = Number.isInteger(timeout) ? timeout : 0;
 
 export default async function Home() {
   const entryUrl = "/";
